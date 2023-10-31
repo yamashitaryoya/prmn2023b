6c7659a54b0c15479b7eeb80adeb362ac83ca948
[yamashitaryoya] <[ryoyan1022@gmail.com]>
2023-10-31 18:29:30 +0900

    レッスン1

---
+++ prmn2023b/src/main/java/com/example/prmn2023b/Prmn2023bApplication.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2023bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2023bApplication.class, args);
+    }
+
+}
---
+++ prmn2023b/src/test/java/com/example/prmn2023b/Prmn2023bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2023bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
